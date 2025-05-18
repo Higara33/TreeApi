010a8dd7a3e64205d46d7f56ce0db8d78e58266b
Андрей Кузьмин <duha3222@gmail.com>
2025-05-18 14:05:44 +0200

    TESTAPI-001 - Add migrations

---
+++ JournalService/Migrations/20250518120214_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace JournalService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JournalEntries",
+                columns: table => new
+                {
+                    EventId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TimeStamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    PartnerCode = table.Column<string>(type: "text", nullable: false),
+                    QueryParams = table.Column<string>(type: "text", nullable: false),
+                    BodyParams = table.Column<string>(type: "text", nullable: false),
+                    StackTrace = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JournalEntries", x => x.EventId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JournalEntries");
+        }
+    }
+}
---
+++ TreeService/Migrations/20250518120242_InitialCreate.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TreeService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NodeClosures",
+                columns: table => new
+                {
+                    AncestorId = table.Column<long>(type: "bigint", nullable: false),
+                    DescendantId = table.Column<long>(type: "bigint", nullable: false),
+                    Depth = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NodeClosures", x => new { x.AncestorId, x.DescendantId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TreeId = table.Column<long>(type: "bigint", nullable: false),
+                    ParentNodeId = table.Column<long>(type: "bigint", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Nodes_Nodes_ParentNodeId",
+                        column: x => x.ParentNodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Nodes_Trees_TreeId",
+                        column: x => x.TreeId,
+                        principalTable: "Trees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_ParentNodeId",
+                table: "Nodes",
+                column: "ParentNodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_Tree_Parent_Name",
+                table: "Nodes",
+                columns: new[] { "TreeId", "ParentNodeId", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NodeClosures");
+
+            migrationBuilder.DropTable(
+                name: "Nodes");
+
+            migrationBuilder.DropTable(
+                name: "Trees");
+        }
+    }
+}
